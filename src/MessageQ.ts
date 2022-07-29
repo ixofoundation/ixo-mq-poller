31ddeb3beab2733e9bc96e3323a8f735f8a04ca8
Casey Pazera <ParkourPunk@googlemail.com>
2022-07-29 13:49:12 +0100

    converted broadcast mode from rest to RPC

--- src/MessageQ.ts
+++ src/MessageQ.ts
@@ -1,12 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
+import { BroadcastMode } from "./codec/external/cosmos/tx/v1beta1/service";
+import * as transactions from "./protoquery/transactions";
+import { JsonToArray } from "./protoquery/utils";
 
-const amqplib = require('amqplib');
-const dateFormat = require('dateformat');
+const amqplib = require("amqplib");
+const dateFormat = require("dateformat");
 
-const BLOCKCHAIN_REST = (process.env.BLOCKCHAIN_REST || '');
+const BLOCKCHAIN_REST = process.env.BLOCKCHAIN_REST || "";
 
 export class MessageQ {
-
   connection: any;
   private queue: string;
 
@@ -21,79 +23,120 @@ export class MessageQ {
   start(): Promise<any> {
     const inst = this;
     return new Promise(function (resolve: Function, reject: Function) {
-      amqplib.connect(process.env.RABITMQ_URI || '')
-        .then((conn: any) => {
+      amqplib.connect(process.env.RABITMQ_URI || "").then(
+        (conn: any) => {
           inst.connection = conn;
-          inst.connection.createChannel()
-            .then((ch: any) => {
-              ch.assertExchange("pds.ex", "direct", {durable: true});
-              ch.bindQueue(inst.queue, 'pds.ex');
-              ch.prefetch(50);
-              ch.assertQueue(inst.queue, {durable: true})
-              ch.consume(inst.queue, (messageData: any) => {
-                if (messageData === null) {
-                  return;
+          inst.connection.createChannel().then((ch: any) => {
+            ch.assertExchange("pds.ex", "direct", { durable: true });
+            ch.bindQueue(inst.queue, "pds.ex");
+            ch.prefetch(50);
+            ch.assertQueue(inst.queue, { durable: true });
+            ch.consume(inst.queue, (messageData: any) => {
+              if (messageData === null) {
+                return;
+              }
+              const message = JSON.parse(messageData.content.toString());
+              inst.handleMessage(message.data).then(
+                (response) => {
+                  const msgResponse = {
+                    msgType: message.data.msgType,
+                    txHash: message.txHash,
+                    data: response,
+                  };
+                  console.log(
+                    inst.dateTimeLogger() +
+                      " return blockchain response message " +
+                      message.txHash
+                  );
+                  ch.sendToQueue(
+                    "pds.res",
+                    Buffer.from(JSON.stringify(msgResponse)),
+                    {
+                      persistent: false,
+                      contentType: "application/json",
+                    }
+                  );
+                  return ch.ack(messageData);
+                },
+                (error) => {
+                  ch.sendToQueue(
+                    "pds.res",
+                    Buffer.from(
+                      JSON.stringify({
+                        msgType: "error",
+                        data: error,
+                        txHash: message.txHash,
+                      })
+                    ),
+                    {
+                      persistent: false,
+                      contentType: "application/json",
+                    }
+                  );
+                  return ch.ack(messageData);
                 }
-                const message = JSON.parse(messageData.content.toString());
-                inst.handleMessage(message.data)
-                  .then((response) => {
-                    const msgResponse = {
-                      msgType: message.data.msgType,
-                      txHash: message.txHash,
-                      data: response
-                    };
-                    console.log(inst.dateTimeLogger() + ' return blockchain response message ' + message.txHash);
-                    ch.sendToQueue('pds.res', Buffer.from(JSON.stringify(msgResponse)), {
-                      persistent: false,
-                      contentType: 'application/json'
-                    });
-                    return ch.ack(messageData);
-                  }, (error) => {
-                    ch.sendToQueue('pds.res', Buffer.from(JSON.stringify({
-                      msgType: "error",
-                      data: error,
-                      txHash: message.txHash
-                    })), {
-                      persistent: false,
-                      contentType: 'application/json'
-                    });
-                    return ch.ack(messageData);
-                  });
-              })
-            })
-          console.log(inst.dateTimeLogger() + ' RabbitMQ connected');
+              );
+            });
+          });
+          console.log(inst.dateTimeLogger() + " RabbitMQ connected");
           resolve(conn);
-        }, () => {
+        },
+        () => {
           throw new Error("Cannot connect to RabbitMQ Server");
-        });
+        }
+      );
     });
   }
 
   private handleMessage(message: any): Promise<any> {
     return new Promise((resolve: Function, reject: Function) => {
-      console.log(this.dateTimeLogger() + ' consume from queue ' + JSON.stringify(message));
-      if (message.msgType === 'eth') {
-        console.log(this.dateTimeLogger() + ' skipping eth message')
+      console.log(
+        this.dateTimeLogger() + " consume from queue " + JSON.stringify(message)
+      );
+      if (message.msgType === "eth") {
+        console.log(this.dateTimeLogger() + " skipping eth message");
       } else {
-        const broadcastUrl = BLOCKCHAIN_REST + "/txs"
-        console.log(this.dateTimeLogger() + ' sending message to ' + broadcastUrl);
-        axios.post(broadcastUrl, message.data)
-          .then((response: any) => {
-            if (response.data && response.data.error) {
-              console.log(this.dateTimeLogger() + ' received error response from blockchain ' + JSON.stringify(response.data));
-              reject(response.data.error.data || "Unknown error");
-            } else {
-              console.log(this.dateTimeLogger() + ' received response from blockchain ' + response.data.txhash);
-              resolve(response.data);
-            }
-          })
-          .catch((reason) => {
-            console.log(this.dateTimeLogger() + ' no response from blockchain ' + reason);
-            reject(reason);
+        const broadcastUrl = BLOCKCHAIN_REST + "/txs";
+        console.log(
+          this.dateTimeLogger() + " sending message to " + broadcastUrl
+        );
+        transactions
+          .ServiceBroadcastTx(
+            JsonToArray(message.data),
+            BroadcastMode.BROADCAST_MODE_UNSPECIFIED
+          )
+          .then((response) => {
+            console.log(response);
           });
+
+        // axios
+        //   .post(broadcastUrl, message.data)
+        //   .then((response: any) => {
+        //     if (response.data && response.data.error) {
+        //       console.log(
+        //         this.dateTimeLogger() +
+        //           " received error response from blockchain " +
+        //           JSON.stringify(response.data)
+        //       );
+        //       reject(response.data.error.data || "Unknown error");
+        //     } else {
+        //       console.log(
+        //         this.dateTimeLogger() +
+        //           " received response from blockchain " +
+        //           response.data.txhash
+        //       );
+        //       resolve(response.data);
+        //     }
+        //   })
+        //   .catch((reason) => {
+        //     console.log(
+        //       this.dateTimeLogger() + " no response from blockchain " + reason
+        //     );
+        //     reject(reason);
+        //   });
       }
     });
   }
 }
 
-export default new MessageQ('pds');
+export default new MessageQ("pds");
---
+++ src/protoquery/utils.ts
@@ -0,0 +1,94 @@
+import { fromBase64 } from "@cosmjs/encoding";
+import {
+  OfflineSigner,
+  DirectSecp256k1HdWallet,
+  Registry,
+  decodeTxRaw,
+} from "@cosmjs/proto-signing";
+import { SigningStargateClient } from "@cosmjs/stargate";
+
+export async function initializesigner(
+  Mnemonic: string
+): Promise<OfflineSigner> {
+  const signer = await DirectSecp256k1HdWallet.fromMnemonic(Mnemonic, {
+    prefix: "ixo",
+  });
+
+  return signer;
+}
+
+export async function initializeclient(
+  signer: OfflineSigner,
+  rpcendpoint = "https://testnet.ixo.world/rpc/",
+  myRegistry: any
+): Promise<SigningStargateClient> {
+  const client = await SigningStargateClient.connectWithSigner(
+    rpcendpoint,
+    signer,
+    { registry: myRegistry }
+  );
+
+  return client;
+}
+
+export async function parseTx(tx: string, registry: Registry): Promise<any> {
+  const decoded = decodeTxRaw(fromBase64(tx));
+  const parsedData = [];
+  for (const message of decoded.body.messages) {
+    const decodedMsg = registry.decode(message);
+    parsedData.push(decodedMsg);
+  }
+  return parsedData;
+}
+
+// JSON to Uint8Array parsing and visa versa
+export const JsonToArray = function (json) {
+  const ret = new Uint8Array(Buffer.from(json));
+  return ret;
+};
+
+function Utf8ArrayToStr(array: any) {
+  let out, i, c;
+  let char2, char3;
+
+  out = "";
+  const len = array.length;
+  i = 0;
+  while (i < len) {
+    c = array[i++];
+    switch (c >> 4) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+        // 0xxxxxxx
+        out += String.fromCharCode(c);
+        break;
+      case 12:
+      case 13:
+        // 110x xxxx   10xx xxxx
+        char2 = array[i++];
+        out += String.fromCharCode(((c & 0x1f) << 6) | (char2 & 0x3f));
+        break;
+      case 14:
+        // 1110 xxxx  10xx xxxx  10xx xxxx
+        char2 = array[i++];
+        char3 = array[i++];
+        out += String.fromCharCode(
+          ((c & 0x0f) << 12) | ((char2 & 0x3f) << 6) | ((char3 & 0x3f) << 0)
+        );
+        break;
+    }
+  }
+
+  return out;
+}
+// Converts A Unit8Array to String
+export function Uint8ArrayToJS(data: Uint8Array): string {
+  const decodedData = Utf8ArrayToStr(data);
+  return decodedData;
+}
